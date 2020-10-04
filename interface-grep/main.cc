3b2006bf567b02d3eaa2758a759b8c8e606dc598
TheDeadFish <michaelstamper1@gmail.com>
2020-10-04 11:46:07 +0000

    macro expansion prep work, WIP

--- interface-grep/main.cc
+++ interface-grep/main.cc
@@ -8,7 +8,8 @@ static FILE* s_fpOut;
 static byte s_prevTok;
 static int s_printFlag;
 static cParse* s_cParse;
-const char* s_curFile;
+static const char* s_curFile;
+static xVector<cParse::Parse_t> s_args;
 
 struct defineName_t {
 	int type; cstr name; 
@@ -16,38 +17,33 @@ struct defineName_t {
 
 struct define_t {
 	cstr name; 
-	xArray<xstr> args;
-	cParse::Parse_t toks; 
+	xarray<cstr> args;
+	cParse::Parse_t toks;
 };
 
 xarray<define_t> defList;
 
 defineName_t parse_defName(
-	xVector<cParse::Parse_t>& args,
 	cParse::Parse_t& pos)
 {
-	// get name
-	cParse::Parse_t	tmp = pos;
+	// get call
 	defineName_t ret = {-1,
-		tmp.fi().cStr()};
-	
-	// get arguments
-	ret.type = tmp.getArgs(args);
-	if(ret.type >= 0) pos = tmp; 
+		pos.getCall2(s_args)};
+	if(ret.name.slen)
+		ret.type = cParse::fixArgs(s_args);
 	return ret;
 }
 
-/*
 define_t* lookup_define(defineName_t& name)
 {
+	int nArgs = name.type>0 ? s_args.size() : -1;
+
 	for(auto& def : defList) {
-		if(def.name.type == name.type
-		&&(!def.name.name.cmp(name.name)))
+		if(def.args.len == nArgs
+		&&(!def.name.cmp(name.name)))
 			return &def;
 	} return NULL;
 }
-
-/*
 
 void parse_define(cParse::Parse_t pos)
 {
@@ -59,16 +55,22 @@ void parse_define(cParse::Parse_t pos)
 	// skip defines without STDMETHOD_
 	if(!pos.text().str("STDMETHOD_"))
 		return;
-
 	// lookup/allocate item
 	define_t* x = lookup_define(name);
 	if(!x) { x = &defList.xnxcalloc(); 
-		x->name = name; }
+		x->name = name.name; }
 
 	// update item
 	x->toks = pos;
 
-} */
+	// copy args
+	if(name.type == 0) {
+		x->args.len = -1;
+	} else {
+		for(auto& arg : s_args)
+			x->args.push_back(arg->cStr());
+	}
+}
 
 void print_tokens(cParse::Parse_t x, int flags)
 {
@@ -90,7 +92,6 @@ void error(char* str, cch* fmt, ...)
 
 void parse_methods(cParse::Parse_t pos)
 {
-	xVector<cParse::Parse_t> args;
 	static const cParse::Token hresult = 
 		cParse::Token::make(CTOK_NAME, "HRESULT");
 
@@ -99,25 +100,23 @@ void parse_methods(cParse::Parse_t pos)
 			// skip operators
 			if(pos->value() != CTOK_NAME) {
 				pos.fi(); continue; }
-				
-			printf("!!! %.*s\n", pos->cStr().prn());
-				
+
 			// parse name
-			auto item = parse_defName(args, pos);
+			auto item = parse_defName(pos);
 			if(item.type < 0) { 
 				error(pos->str, "bad args"); break; }
 
 			// STDMETHOD_
-			if((args.size() != 2)
+			if((s_args.size() != 2)
 			||(item.name.cmp("STDMETHOD_"))) {
 			
 			
 				// STDMETHOD
-				if((args.size() == 1)
+				if((s_args.size() == 1)
 				&&(!item.name.cmp("STDMETHOD"))) {
-					args.xresize(2);
-					args[1] = args[0];
-					args[0] = {(cParse::Token*)&hresult, 1};
+					s_args.xresize(2);
+					s_args[1] = s_args[0];
+					s_args[0] = {(cParse::Token*)&hresult, 1};
 					
 				} else {
 				
@@ -125,33 +124,23 @@ void parse_methods(cParse::Parse_t pos)
 					if((!item.name.cmp("PURE"))
 					||(!item.name.cmp("DEFINE_ABSTRACT_UNKNOWN")))
 						continue;
-						
-					// lookup macro
-					
-					
-					
-					
-						
-				
-				
-					printf("%.*s\n", pos->cStr().prn());
-				
-				
-				
-				
-					error(pos->str, "bad function"); 
+
+					auto* macro = lookup_define(item);
+					if(macro) parse_methods(macro->toks);
+					else { error(item.name, "bad function: %.*s", 
+						item.name.prn()); }
 					continue;
 				}
 			}
 			
 			// method type and name
-			print_tokens(args[0], s_printFlag);
+			print_tokens(s_args[0], s_printFlag);
 			s_printFlag = 3;
-			print_tokens(args[1], 5);
+			print_tokens(s_args[1], 5);
 			
 			// method arguments
-			pos.getArgs(args);
-			for(auto& x : args) 
+			pos.getArgs(s_args);
+			for(auto& x : s_args)
 			{
 				// skip THIS argument
 				if(!x->cStr().cmp("THIS_")
@@ -159,7 +148,7 @@ void parse_methods(cParse::Parse_t pos)
 				if(!x.chk()) continue;
 				
 				// print argument
-				print_tokens(x, args.data() != &x);
+				print_tokens(x, s_args.data() != &x);
 			}
 
 			fputs("]]", s_fpOut);
@@ -170,7 +159,6 @@ void parse_methods(cParse::Parse_t pos)
 void parse_interface(cch* name)
 {
 	cParse cp;
-	xVector<cParse::Parse_t> args;
 	
 	if(!cp.load2(name, 0)) {
 	
@@ -182,20 +170,24 @@ void parse_interface(cch* name)
 		while(pos.chk())
 		{
 			// parse any macros
-			//if(pos.cppType() == CPP_DEFINE) {
-			//	parse_define(pos.cppBlock());
-			//	continue;
-			//}
+			if(pos.cppType() == CPP_DEFINE) {
+				parse_define(pos.cppBlock());;
+				continue;
+			}
 			
+			// skip operators
+			if(pos->value() != CTOK_NAME) {
+				pos.fi(); continue; }
+
 			// get interface
-			cstr name = pos.getCall(args);
-			if(args.size() != 2) continue;
+			cstr name = pos.getCall2(s_args);
+			if(s_args.size() != 2) continue;
 			if(name.cmp("DECLARE_INTERFACE_")) 
 				continue;
 				
 			// print interface
-			print_tokens(args[0], 2);
-			print_tokens(args[1], 5);
+			print_tokens(s_args[0], 2);
+			print_tokens(s_args[1], 5);
 		
 			// parse methods
 			s_printFlag = 2;
